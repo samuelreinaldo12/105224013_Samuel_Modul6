6a76487b4ddbdead4f2f3dc291f360b0c627168b
Samuelreinaldo12 <samuelreinaldo30@gmail.com>
2026-04-07 23:54:19 +0700

    LATIHAN

--- LatihanModul6/Main.java
+++ LatihanModul6/Main.java
@@ -1,9 +1,12 @@
-package game;
+import game.Hero;
+import game.HeroStrength;
 
 public class Main {
     public static void main(String[] args) {
-        Hero hero1 = new Hero("Claude", 100.0, 30.0);
-        HeroStrength hero2 = new HeroStrength("Gatotkaca", 150.0, 15.0);
+        // Soal 5: Membuat dua objek Hero yang berbeda
+        // Bebas berkreasi dengan nama dan status awalnya!
+        Hero hero1 = new Hero("Arthur", 100.0, 20.0);
+        HeroStrength hero2 = new HeroStrength("Gatotkaca", 150.0, 25.0);
 
         System.out.println("=== STATUS AWAL ===");
         hero1.display();
@@ -15,9 +18,11 @@ public class Main {
         hero2.berlatih();
 
         System.out.println("\n=== SESI PERTARUNGAN ===");
+        // Mari kita simulasikan keduanya terkena serangan sebesar 30 damage
         double seranganMusuh = 30.0;
         hero1.terimaSerangan(seranganMusuh);
         hero2.terimaSerangan(seranganMusuh); 
+        // hero2 seharusnya hanya menerima 15 damage karena overridenya
 
         System.out.println("\n=== STATUS AKHIR ===");
         hero1.display();
